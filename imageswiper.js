cfee20b10e4aaa9ef04e17cea9762b72b66b4ca7
Jatinverma9728 <somveerparjapati123456@gmail.com>
2024-10-21 22:12:50 +0530

    day 38

--- imageswiper.js
+++ imageswiper.js
@@ -1,9 +1,8 @@
 const swiper = new Swiper(".slider-wrapper", {
   slidesPerView: 3,
   spaceBetween: 30,
-  loop: false, // Enable looping
+  loop: false,
   watchOverflow: true,
-  // grabCursor: true,
 
   pagination: {
     el: ".swiper-pagination",
@@ -11,5 +10,34 @@ const swiper = new Swiper(".slider-wrapper", {
     dynamicBullets: true,
   },
 
-
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      centeredSlides: false, // Disable centered slides to avoid partial slides
+      slidesPerGroup: 1, // Swipe 1 card at a time
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      centeredSlides: false,
+      slidesPerGroup: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      centeredSlides: false,
+      slidesPerGroup: 1,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      centeredSlides: false,
+      slidesPerGroup: 1,
+    },
+  },
 });
