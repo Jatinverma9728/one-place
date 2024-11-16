30c04abc266d3fec34c822a4cf031caa7f662c52
Jatinverma9728 <somveerparjapati123456@gmail.com>
2024-11-16 21:23:23 +0530

    day 54

--- imageswiper.js
+++ imageswiper.js
@@ -13,14 +13,14 @@ const swiper = new Swiper(".slider-wrapper", {
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 1,
+      slidesPerView: 2,
       spaceBetween: 10,
       centeredSlides: false, // Disable centered slides to avoid partial slides
       slidesPerGroup: 1, // Swipe 1 card at a time
     },
     // when window width is >= 480px
     480: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 20,
       centeredSlides: false,
       slidesPerGroup: 1,
